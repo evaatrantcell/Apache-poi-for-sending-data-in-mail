d8ca3e9f3525c5305a01e89516cb01d0286d3c77
DarshanUTHUK <126103647+uth-testautomation@users.noreply.github.com>
2024-09-19 14:22:29 +0530

    updated code for mail result

--- vars/mylibrary.groovy
+++ vars/mylibrary.groovy
@@ -5,34 +5,50 @@ import java.io.File
 import java.io.FileInputStream
 
 def call(filePath, sheetName) {
-    FileInputStream file = new FileInputStream(new File(filePath))
-    Workbook workbook = new XSSFWorkbook(file)
-    Sheet sheet = workbook.getSheet(sheetName)
+    def f1 = new File(config.test_run_excelreportdata_path)
+    def testrunexcelfolder = f1.text
 
+    // Get all .xlsx files in the directory
+    def files = new File(directoryPath).listFiles().findAll { it.name.endsWith(".xlsx") }
+    
     def tableHtml = "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse;'>"
-    for (Row row : sheet) {
-        tableHtml += "<tr>"
-        for (Cell cell : row) {
-            String cellValue = ""
-            switch (cell.cellType) {
-                case CellType.STRING:
-                    cellValue = cell.stringCellValue
-                    break
-                case CellType.NUMERIC:
-                    cellValue = cell.numericCellValue.toString()
-                    break
-                case CellType.BOOLEAN:
-                    cellValue = cell.booleanCellValue.toString()
-                    break
-                default:
-                    cellValue = ""
+    files.each { file ->
+        FileInputStream fis = new FileInputStream(file)
+        XSSFWorkbook workbook = new XSSFWorkbook(fis)
+        
+        // Check if the sheet exists in the workbook
+        if (workbook.getSheet(sheetName)) {
+            println "Sheet '${sheetName}' found in file: ${file.getAbsolutePath()}"
+            Sheet sheet = workbook.getSheet(sheetName)
+            for (Row row : sheet) {
+                tableHtml += "<tr>"
+                for (Cell cell : row) {
+                    String cellValue = ""
+                    switch (cell.cellType) {
+                        case CellType.STRING:
+                            cellValue = cell.stringCellValue
+                            break
+                        case CellType.NUMERIC:
+                            cellValue = cell.numericCellValue.toString()
+                            break
+                        case CellType.BOOLEAN:
+                            cellValue = cell.booleanCellValue.toString()
+                            break
+                        default:
+                            cellValue = ""
+                    }
+                    tableHtml += "<td>${cellValue}</td>"
+                }
+                tableHtml += "</tr>"
             }
-            tableHtml += "<td>${cellValue}</td>"
-        }
-        tableHtml += "</tr>"
+            tableHtml += "</table>"
+            }
+        workbook.close()
+        fis.close()
     }
-    tableHtml += "</table>"
-
-    file.close()
+    // FileInputStream file = new FileInputStream(new File(filePath))
+    // Workbook workbook = new XSSFWorkbook(file)
+    // Sheet sheet = workbook.getSheet(sheetName)
+    // file.close()
     return tableHtml
 }
