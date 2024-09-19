073d91c1d0fb9339671af6f5a5f596f1daf6ed64
DarshanUTHUK <126103647+uth-testautomation@users.noreply.github.com>
2024-09-19 14:52:39 +0530

    updated code for mail result

--- vars/mylibrary.groovy
+++ vars/mylibrary.groovy
@@ -4,13 +4,12 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileInputStream
 
-def call(filePath, sheetName) {
+def call(filePath, sheetName, excelfolder) {
     def f1 = new File(filePath)
     def testrunexcelfolder = f1.text
-
+    def excel_report_file_path = excelfolder + testrunexcelfolder
     // Get all .xlsx files in the directory
-    def files = new File(directoryPath).listFiles().findAll { it.name.endsWith(".xlsx") }
-    
+    def files = new File(excel_report_file_path).listFiles().findAll { it.name.endsWith(".xlsx") }
     def tableHtml = "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse;'>"
     files.each { file ->
         FileInputStream fis = new FileInputStream(file)
@@ -19,7 +18,7 @@ def call(filePath, sheetName) {
         // Check if the sheet exists in the workbook
         if (workbook.getSheet(sheetName)) {
             println "Sheet '${sheetName}' found in file: ${file.getAbsolutePath()}"
-            Sheet sheet = workbook.getSheet(sheetName)
+            // List to store file paths
             for (Row row : sheet) {
                 tableHtml += "<tr>"
                 for (Cell cell : row) {
@@ -41,14 +40,14 @@ def call(filePath, sheetName) {
                 }
                 tableHtml += "</tr>"
             }
-            tableHtml += "</table>"
-            }
+        }
         workbook.close()
         fis.close()
     }
-    // FileInputStream file = new FileInputStream(new File(filePath))
-    // Workbook workbook = new XSSFWorkbook(file)
-    // Sheet sheet = workbook.getSheet(sheetName)
-    // file.close()
+    tableHtml += "</table>"
+//     FileInputStream file = new FileInputStream(new File(filePath))
+//     Workbook workbook = new XSSFWorkbook(file)
+//     Sheet sheet = workbook.getSheet(sheetName)
+//     file.close()
     return tableHtml
 }
